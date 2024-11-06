b1ba2a68db505ad2a0a6b189eaa86fb20d38b816
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-11-06 16:53:05 -0300

    fix: retirando os comentarios do arquivo js

--- app/src/index.js
+++ app/src/index.js
@@ -17,9 +17,7 @@ function toggleMobileMenu() {
   console.log("clicou");
 }
 
-// Função para exibir o slide atual
 function showSlide(index) {
-  // Ajustar o índice para garantir que está no intervalo correto
   if (index >= slides.length) {
     currentSlideIndex = 0;
   } else if (index < 0) {
@@ -28,7 +26,6 @@ function showSlide(index) {
     currentSlideIndex = index;
   }
 
-  // Ocultar todos os slides e remover a classe 'active' dos dots
   slides.forEach((slide) => {
     slide.style.display = "none";
   });
@@ -37,24 +34,20 @@ function showSlide(index) {
     dot.classList.remove("active");
   });
 
-  // Mostrar o slide atual e adicionar a classe 'active' ao dot correspondente
   slides[currentSlideIndex].style.display = "block";
   dots[currentSlideIndex].classList.add("active");
 }
 
-// Função para avançar para o próximo slide
 function nextSlide() {
   showSlide(currentSlideIndex + 1);
 }
 
-// Função para voltar ao slide anterior
 function prevSlide() {
   showSlide(currentSlideIndex - 1);
 }
 
-// Função para ir para um slide específico
 function currentSlide(index) {
-  showSlide(index - 1); // O índice dos dots começa em 1, por isso precisamos subtrair 1
+  showSlide(index - 1);
 }
 
 // Inicializar o carrossel mostrando o primeiro slide
