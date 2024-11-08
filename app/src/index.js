5b138992101d07d25225d20493634b793e77c2c3
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-11-08 14:56:52 -0300

    feat: finalizando o footer com a validação do campo email

--- app/src/index.js
+++ app/src/index.js
@@ -2,21 +2,21 @@ let currentSlideIndex = 0;
 const slides = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll(".dot");
 
+// Função para alternar o menu mobile
 function toggleMobileMenu() {
   const mobileMenu = document.getElementById("mobile-menu");
-
   if (mobileMenu) {
     const isMenuVisible = mobileMenu.classList.contains("hidden");
-
     if (isMenuVisible) {
-      mobileMenu.classList.remove("hidden"); // Mostra o menu
+      mobileMenu.classList.remove("hidden");
     } else {
-      mobileMenu.classList.add("hidden"); // Esconde o menu
+      mobileMenu.classList.add("hidden");
     }
   }
   console.log("clicou");
 }
 
+// Função para mostrar o slide atual
 function showSlide(index) {
   if (index >= slides.length) {
     currentSlideIndex = 0;
@@ -38,6 +38,7 @@ function showSlide(index) {
   dots[currentSlideIndex].classList.add("active");
 }
 
+// Funções para avançar e retroceder slides
 function nextSlide() {
   showSlide(currentSlideIndex + 1);
 }
@@ -50,5 +51,29 @@ function currentSlide(index) {
   showSlide(index - 1);
 }
 
+// Função para iniciar o carrossel automático
+function startAutoSlide() {
+  setInterval(() => {
+    nextSlide();
+  }, 5000);
+}
+
+// Função para verificar o formato do email e mostrar uma confirmação
+function validateEmail() {
+  const emailInput = document.getElementById("email");
+  const emailValue = emailInput.value;
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailPattern.test(emailValue)) {
+    return confirm(`Enviaremos um email de contato para: ${emailValue}`);
+  } else {
+    alert("Por favor, insira um email válido.");
+    emailInput.classList.add("invalid");
+    emailInput.classList.remove("valid");
+    return false; // Impede o envio do formulário
+  }
+}
+
 // Inicializar o carrossel mostrando o primeiro slide
 showSlide(currentSlideIndex);
+startAutoSlide();
